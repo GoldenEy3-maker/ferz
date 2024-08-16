ccbe2213a59f53d88b2d5dc15ddf7d2a69c81560
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2024-08-16 12:24:46 -0700

    new-product-page

--- scripts/main.js
+++ scripts/main.js
@@ -115,316 +115,295 @@ function initializeAutoresizes() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (window.location.hash) {
-    lenis.scrollTo(window.location.hash);
-  }
+if (window.location.hash) {
+  lenis.scrollTo(window.location.hash);
+}
 
-  const navLinks = document.querySelectorAll(".header-nav a");
-  if (navLinks.length)
-    navLinks.forEach((link) => {
-      link.addEventListener("click", (event) => {
-        const hash = link.href.slice(link.href.lastIndexOf("#"));
-        lenis.scrollTo(hash);
-      });
+const navLinks = document.querySelectorAll(".header-nav a");
+if (navLinks.length)
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      const hash = link.href.slice(link.href.lastIndexOf("#"));
+      lenis.scrollTo(hash);
+    });
+  });
+
+const btnTitles = document.querySelectorAll(".btn--title");
+if (btnTitles.length) {
+  btnTitles.forEach((btn) => {
+    btn.addEventListener("mouseenter", () => {
+      if (!btn.disabled)
+        anime({
+          targets: btn,
+          scale: 1.1,
+        });
     });
 
-  // const btnArrowAnim = document.querySelectorAll(".btn-arrow-anim");
-  // if (btnArrowAnim.length) {
-  //   btnArrowAnim.forEach((btn) => {
-  //     const icon = btn.querySelector("svg");
-  //     btn.addEventListener("mouseenter", () => {
-  //       if (!btn.disabled) {
-  //         anime({
-  //           targets: icon,
-  //           translateY: [0, -5, 0],
-  //           translateX: [0, 5, 0],
-  //         });
-  //       }
-  //     });
-  //   });
-  // }
-
-  const btnTitles = document.querySelectorAll(".btn--title");
-  if (btnTitles.length) {
-    btnTitles.forEach((btn) => {
-      btn.addEventListener("mouseenter", () => {
-        if (!btn.disabled)
-          anime({
-            targets: btn,
-            scale: 1.1,
-          });
-      });
-
-      btn.addEventListener("mouseleave", () => {
-        if (!btn.disabled)
-          anime({
-            targets: btn,
-            scale: 1,
-          });
-      });
+    btn.addEventListener("mouseleave", () => {
+      if (!btn.disabled)
+        anime({
+          targets: btn,
+          scale: 1,
+        });
     });
-  }
-  const paralaxMouseRoots = document.querySelectorAll(
-    "[data-paralax-mouse-root]"
+  });
+}
+const paralaxMouseRoots = document.querySelectorAll(
+  "[data-paralax-mouse-root]"
+);
+
+if (paralaxMouseRoots.length) {
+  paralaxMouseRoots.forEach((root) => {
+    const layers = root.querySelectorAll("[data-paralax-layer]");
+
+    function handleParallax(evt) {
+      const parallaxLeftOffset = root.getBoundingClientRect().left;
+      const parallaxTopOffset = root.getBoundingClientRect().top;
+
+      const coordX = evt.clientX - parallaxLeftOffset - 0.5 * root.offsetWidth;
+      const coordY = evt.clientY - parallaxTopOffset - 0.5 * root.offsetHeight;
+
+      layers.forEach((layer) => {
+        const layerSpeed = layer.dataset.speed;
+        const x = -(coordX * layerSpeed).toFixed(2);
+        const y = -(coordY * layerSpeed).toFixed(2);
+        layer.setAttribute("style", `transform: translate(${x}px, ${y}px);`);
+      });
+    }
+
+    function reset(event) {
+      layers.forEach((layer) => {
+        layer.removeAttribute("style");
+      });
+    }
+
+    root.addEventListener("mousemove", handleParallax);
+    root.addEventListener("mouseleave", reset);
+  });
+}
+
+gsap.utils.toArray(".image-parallax").forEach((container) => {
+  const img = container.querySelector("img");
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: container,
+      scrub: true,
+      pin: false,
+    },
+  });
+
+  tl.fromTo(
+    img,
+    {
+      yPercent: -15,
+      ease: "none",
+    },
+    {
+      yPercent: 15,
+      ease: "none",
+    }
+  );
+});
+
+const burgerTrigger = document.querySelector("[data-burger-trigger]");
+
+const _burgerAnimationDuration = 300;
+
+function closeBurger() {
+  const root = document.querySelector("[data-burger-root]");
+  if (!root) return;
+  unlockScroll(_burgerAnimationDuration);
+  root.ariaHidden = true;
+  burgerTrigger.focus();
+  setTimeout(() => root.remove(), _burgerAnimationDuration);
+}
+
+function openBurger() {
+  const template = document.querySelector("[data-burger-template]");
+  const templateContent = template.content.cloneNode(true);
+  const closes = templateContent.querySelectorAll("[data-burger-close]");
+  const root = templateContent.querySelector("[data-burger-root]");
+  const focusGuard = templateContent.querySelector("[data-focus-guard]");
+  const focusableElements = templateContent.querySelectorAll(
+    'a, button:not([disabled]), input:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"]), select:not([disabled]), textarea:not([disabled])'
   );
 
-  if (paralaxMouseRoots.length) {
-    paralaxMouseRoots.forEach((root) => {
-      const layers = root.querySelectorAll("[data-paralax-layer]");
-
-      function handleParallax(evt) {
-        const parallaxLeftOffset = root.getBoundingClientRect().left;
-        const parallaxTopOffset = root.getBoundingClientRect().top;
-
-        const coordX =
-          evt.clientX - parallaxLeftOffset - 0.5 * root.offsetWidth;
-        const coordY =
-          evt.clientY - parallaxTopOffset - 0.5 * root.offsetHeight;
-
-        layers.forEach((layer) => {
-          const layerSpeed = layer.dataset.speed;
-          const x = -(coordX * layerSpeed).toFixed(2);
-          const y = -(coordY * layerSpeed).toFixed(2);
-          layer.setAttribute("style", `transform: translate(${x}px, ${y}px);`);
-        });
-      }
-
-      function reset(event) {
-        layers.forEach((layer) => {
-          layer.removeAttribute("style");
-        });
-      }
-
-      root.addEventListener("mousemove", handleParallax);
-      root.addEventListener("mouseleave", reset);
+  if (closes.length) {
+    closes.forEach((close) => {
+      close.addEventListener("click", closeBurger);
     });
   }
 
-  gsap.utils.toArray(".image-parallax").forEach((container) => {
-    const img = container.querySelector("img");
+  lockScroll();
+  root.style.animationDuration = _burgerAnimationDuration + "ms";
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: container,
-        scrub: true,
-        pin: false,
-      },
-    });
+  document.documentElement.appendChild(templateContent);
 
-    tl.fromTo(
-      img,
-      {
-        yPercent: -15,
-        ease: "none",
-      },
-      {
-        yPercent: 15,
-        ease: "none",
-      }
-    );
+  focusableElements?.[0].focus();
+
+  focusGuard.addEventListener("focus", () => {
+    focusableElements?.[0].focus();
   });
 
-  const burgerTrigger = document.querySelector("[data-burger-trigger]");
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") closeBurger();
+  });
+}
 
-  const _burgerAnimationDuration = 300;
+burgerTrigger.addEventListener("click", openBurger);
 
-  function closeBurger() {
-    const root = document.querySelector("[data-burger-root]");
-    if (!root) return;
-    unlockScroll(_burgerAnimationDuration);
-    root.ariaHidden = true;
-    burgerTrigger.focus();
-    setTimeout(() => root.remove(), _burgerAnimationDuration);
-  }
+const sheetOverlay = document.querySelector("[data-sheet-overlay]");
 
-  function openBurger() {
-    const template = document.querySelector("[data-burger-template]");
-    const templateContent = template.content.cloneNode(true);
-    const closes = templateContent.querySelectorAll("[data-burger-close]");
-    const root = templateContent.querySelector("[data-burger-root]");
-    const focusGuard = templateContent.querySelector("[data-focus-guard]");
-    const focusableElements = templateContent.querySelectorAll(
-      'a, button:not([disabled]), input:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"]), select:not([disabled]), textarea:not([disabled])'
-    );
+const _sheetAnimationDuration = 300;
 
-    if (closes.length) {
-      closes.forEach((close) => {
-        close.addEventListener("click", closeBurger);
-      });
-    }
+let _usedSheetTrigger = null;
 
+function removeSheet(sheet) {
+  setTimeout(() => sheet.remove(), _sheetAnimationDuration);
+}
+
+function closeSheet(key) {
+  const sheet = document.querySelector(`[data-sheet-root=${key}]`);
+  if (!sheet) return;
+  sheet.ariaHidden = true;
+  sheetOverlay.ariaHidden = true;
+  if (!document.querySelector("[data-burger-root]"))
+    unlockScroll(_sheetAnimationDuration);
+  removeSheet(sheet);
+  if (_usedSheetTrigger) _usedSheetTrigger.focus();
+}
+
+function openSheet(key, trigger = null) {
+  const alreadyOpenedSheets = document.querySelectorAll("[data-sheet-root]");
+  if (alreadyOpenedSheets.length) {
+    alreadyOpenedSheets.forEach((sheet) => {
+      sheet.ariaHidden = true;
+      removeSheet(sheet);
+    });
+  } else {
+    _usedSheetTrigger = trigger;
     lockScroll();
-    root.style.animationDuration = _burgerAnimationDuration + "ms";
+  }
 
-    document.documentElement.appendChild(templateContent);
+  const template = document.querySelector(`[data-sheet-template=${key}]`);
+  const templateContent = template.content.cloneNode(true);
 
-    focusableElements?.[0].focus();
+  const root = templateContent.querySelector("[data-sheet-root]");
+  const closes = templateContent.querySelectorAll("[data-sheet-close]");
+  const autofocuses = templateContent.querySelectorAll(
+    "[autofocus]:not([disabled])"
+  );
+  const focusGuard = templateContent.querySelector("[data-focus-guard]");
+  const focusableElements = templateContent.querySelectorAll(
+    'a, button:not([disabled]), input:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"]), select:not([disabled]), textarea:not([disabled])'
+  );
 
-    focusGuard.addEventListener("focus", () => {
-      focusableElements?.[0].focus();
-    });
+  root.style.animationDuration = _sheetAnimationDuration + "ms";
 
-    document.addEventListener("keydown", (event) => {
-      if (event.code === "Escape") closeBurger();
+  if (closes.length) {
+    closes.forEach((close) => {
+      close.addEventListener("click", () => {
+        closeSheet(key);
+      });
     });
   }
 
-  burgerTrigger.addEventListener("click", openBurger);
+  sheetOverlay.ariaHidden = false;
 
-  const sheetOverlay = document.querySelector("[data-sheet-overlay]");
+  sheetOverlay.addEventListener("click", () => {
+    closeSheet(key);
+  });
 
-  const _sheetAnimationDuration = 300;
+  document.documentElement.appendChild(templateContent);
 
-  let _usedSheetTrigger = null;
-
-  function removeSheet(sheet) {
-    setTimeout(() => sheet.remove(), _sheetAnimationDuration);
-  }
-
-  function closeSheet(key) {
-    const sheet = document.querySelector(`[data-sheet-root=${key}]`);
-    if (!sheet) return;
-    sheet.ariaHidden = true;
-    sheetOverlay.ariaHidden = true;
-    if (!document.querySelector("[data-burger-root]"))
-      unlockScroll(_sheetAnimationDuration);
-    removeSheet(sheet);
-    if (_usedSheetTrigger) _usedSheetTrigger.focus();
-  }
-
-  function openSheet(key, trigger = null) {
-    const alreadyOpenedSheets = document.querySelectorAll("[data-sheet-root]");
-    if (alreadyOpenedSheets.length) {
-      alreadyOpenedSheets.forEach((sheet) => {
-        sheet.ariaHidden = true;
-        removeSheet(sheet);
-      });
-    } else {
-      _usedSheetTrigger = trigger;
-      lockScroll();
-    }
-
-    const template = document.querySelector(`[data-sheet-template=${key}]`);
-    const templateContent = template.content.cloneNode(true);
-
-    const root = templateContent.querySelector("[data-sheet-root]");
-    const closes = templateContent.querySelectorAll("[data-sheet-close]");
-    const autofocuses = templateContent.querySelectorAll(
-      "[autofocus]:not([disabled])"
-    );
-    const focusGuard = templateContent.querySelector("[data-focus-guard]");
-    const focusableElements = templateContent.querySelectorAll(
-      'a, button:not([disabled]), input:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"]), select:not([disabled]), textarea:not([disabled])'
-    );
-
-    root.style.animationDuration = _sheetAnimationDuration + "ms";
-
-    if (closes.length) {
-      closes.forEach((close) => {
-        close.addEventListener("click", () => {
-          closeSheet(key);
-        });
-      });
-    }
-
-    sheetOverlay.ariaHidden = false;
-
-    sheetOverlay.addEventListener("click", () => {
-      closeSheet(key);
-    });
-
-    document.documentElement.appendChild(templateContent);
-
-    if (autofocuses.length) {
-      autofocuses.forEach((node) => {
-        node.focus({
-          preventScroll: true,
-        });
-      });
-    } else {
-      focusableElements?.[0].focus({
+  if (autofocuses.length) {
+    autofocuses.forEach((node) => {
+      node.focus({
         preventScroll: true,
       });
-    }
-
-    focusGuard.addEventListener("focus", () => {
-      focusableElements?.[0].focus();
     });
-
-    document.addEventListener("keydown", (event) => {
-      if (event.code === "Escape") closeSheet(key);
+  } else {
+    focusableElements?.[0].focus({
+      preventScroll: true,
     });
   }
 
-  document.addEventListener("click", (event) => {
-    const sheetTrigger = event.target.closest("[data-sheet-trigger]");
-    if (sheetTrigger)
-      openSheet(sheetTrigger.dataset.sheetTrigger, sheetTrigger);
+  focusGuard.addEventListener("focus", () => {
+    focusableElements?.[0].focus();
   });
 
-  const accordionItems = document.querySelectorAll("[data-accordion-item]");
-
-  if (accordionItems.length) {
-    accordionItems.forEach((item) => {
-      const trigger = item.querySelector("[data-accordion-trigger]");
-      if (!trigger) return;
-      trigger.addEventListener("click", () => {
-        const isOpen = item.dataset.accordionItem === "true";
-        item.dataset.accordionItem = !isOpen;
-      });
-    });
-  }
-
-  function observeSpanInner(target, delay) {
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          gsap.to(entry.target, {
-            y: "-2%",
-            delay: delay,
-            duration: 0.45,
-          });
-          observer.unobserve(entry.target);
-        }
-      });
-    });
-
-    observer.observe(target);
-  }
-
-  const targetElements = document.querySelectorAll(".span-line-inner");
-
-  targetElements.forEach((target) => {
-    observeSpanInner(target, parseFloat(target.dataset.delay ?? 0));
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") closeSheet(key);
   });
+}
 
-  function observeElements(target, delay) {
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          gsap.from(entry.target, {
-            y: "2em",
-            opacity: 0,
-            duration: 0.5,
-            delay: delay,
-          });
-          observer.unobserve(entry.target);
-        }
-      });
-    });
-
-    observer.observe(target);
-  }
-
-  const targetElementsDownUpAnimations = document.querySelectorAll(".anima");
-
-  if (targetElementsDownUpAnimations.length) {
-    targetElementsDownUpAnimations.forEach((target) =>
-      observeElements(target, parseFloat(target.dataset.delay ?? 0.2))
-    );
-  }
-
-  initializeAutoresizes();
-  initializeTelMask();
+document.addEventListener("click", (event) => {
+  const sheetTrigger = event.target.closest("[data-sheet-trigger]");
+  if (sheetTrigger) openSheet(sheetTrigger.dataset.sheetTrigger, sheetTrigger);
 });
+
+const accordionItems = document.querySelectorAll("[data-accordion-item]");
+
+if (accordionItems.length) {
+  accordionItems.forEach((item) => {
+    const trigger = item.querySelector("[data-accordion-trigger]");
+    if (!trigger) return;
+    trigger.addEventListener("click", () => {
+      const isOpen = item.dataset.accordionItem === "true";
+      item.dataset.accordionItem = !isOpen;
+    });
+  });
+}
+
+function observeSpanInner(target, delay) {
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        gsap.to(entry.target, {
+          y: "-2%",
+          delay: delay,
+          duration: 0.45,
+        });
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
+  observer.observe(target);
+}
+
+const targetElements = document.querySelectorAll(".span-line-inner");
+
+targetElements.forEach((target) => {
+  observeSpanInner(target, parseFloat(target.dataset.delay ?? 0));
+});
+
+function observeElements(target, delay) {
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        gsap.from(entry.target, {
+          y: "2em",
+          opacity: 0,
+          duration: 0.5,
+          delay: delay,
+        });
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
+  observer.observe(target);
+}
+
+const targetElementsDownUpAnimations = document.querySelectorAll(".anima");
+
+if (targetElementsDownUpAnimations.length) {
+  targetElementsDownUpAnimations.forEach((target) =>
+    observeElements(target, parseFloat(target.dataset.delay ?? 0.2))
+  );
+}
+
+initializeAutoresizes();
+initializeTelMask();
